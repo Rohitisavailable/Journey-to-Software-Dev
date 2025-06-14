a1c7201d3b29194c5e019323d8e294d24e7e43db
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-14 12:15:06 +0530

    first commit

---
+++ 01.BASIC SYNTAX/01. first-program.py
@@ -0,0 +1,14 @@
+print("HELLO WORLD")
+#print() is a function that outputs the string to the console
+#" "# is a string literal that contains the text to be printed"
+
+print('HELLO WORLD AGAIN')
+# ' ' is a string literal that contains the text to be printed
+print("HELLO WORLD AGAIN AND AGAIN")
+print("HELLO WORLD AGAIN AND AGAIN AND AGAIN")
+print(""" HELLO WORLD AGAIN AND AGAIN AND AGAIN AND AGAIN
+HELLO WORLD AGAIN AND AGAIN AND AGAIN AND AGAINHELLO WORLD AGAIN AND AGAIN AND AGAIN AND AGAIN""")
+print("HELLO WORLD AGAIN AND AGAIN AND AGAIN AND AGAIN")
+
+#""" """# is a multi-line string literal that can span multiple lines
+# This code prints "HELLO WORLD" multiple times using different string literals.
---
+++ 01.BASIC SYNTAX/03. variables.py
@@ -0,0 +1,52 @@
+x = "john"
+#x is 
+n = "Abby"
+#1.
+name = "John"
+#name is a variable that stores the string "John"
+print(name)
+# print() is a function that outputs the value of the variable to the console
+#name is a variable that stores the string "John"
+# "John" is a string literal that contains the text to be printed
+
+#2.
+name = "steve"
+#name is a variable that stores the string "steve"
+print(name)
+# print() is a function that outputs the value of the variable to the console
+#name is a variable that stores the string "steve"
+# "steve" is a string literal that contains the text to be printed
+
+#3.
+name = "Abby"
+#name is a variable that stores the string "Abby"
+print(name)
+# print() is a function that outputs the value of the variable to the console
+#name is a variable that stores the string "Abby"
+# "Abby" is a string literal that contains the text to be printed
+
+
+temprature = 30
+# temprature is a variable that stores the integer 30
+
+print(temprature)
+# print() is a function that outputs the value of the variable to the console   
+# temprature is a variable that stores the integer 30
+# 30 is an integer literal that contains the value to be printed
+
+
+#variable should start with a letter or underscore
+#variable names can contain letters, numbers, and underscores
+#variable names are case-sensitive
+#for example,
+
+first_name = "Mark"
+last_name = "Smith"
+# first_name and last_name are variables that store the strings "Mark" and "Smith" respectively
+
+print(first_name, last_name)
+
+
+#summary:
+# Variables are used to store data values.
+# shor
