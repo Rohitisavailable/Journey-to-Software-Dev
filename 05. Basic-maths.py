9fed0b12e6d1e78d3fd78b4684ac0fbbe27487df
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-15 13:03:57 +0530

    added basic-maths file with addition, subtraction, multiplication, and division functions

---
+++ 05. Basic-maths.py
@@ -0,0 +1,47 @@
+a = 2
+# a is a variable that stores the integer 2
+
+b = 4
+# b is a variable that stores the integer 4
+
+# a and b are variables that store integer values
+############################################################################
+#1. for addition
+c = a + b
+# c is a variable that stores the result of adding a and b
+print(c)
+############################################################################
+#2. for subtraction
+
+d = a - b
+# d is a variable that stores the result of subtracting b from a
+print(d)
+############################################################################
+#3. for multiplication
+
+e = a * b
+# e is a variable that stores the result of multiplying a and b
+# * is the multiplication operator
+print(e)
+############################################################################
+#4. for division
+
+f = a / b
+# f is a variable that stores the result of dividing a by b
+# / is the division operator
+print(f)
+############################################################################
+
+
+g = a + b - a * b / a
+# g is a variable that stores the result of the expression a + b - a * b / a
+# This expression combines addition, subtraction, multiplication, and division
+# The order of operations is followed: multiplication and division are performed before addition and subtraction
+# The result is calculated as follows:
+# 1. a * b = 2 * 4 = 8
+# 2. a + b = 2 + 4 = 6
+# 3. 8 / a = 8 / 2 = 4  
+# 4. 6 - 4 = 2
+# g is a variable that stores the final result of the expression
+
+print(g)
