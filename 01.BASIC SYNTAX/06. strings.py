851e5dab0b4d8d9e546b61cbfa4af8433560c873
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-15 13:10:32 +0530

    added strings.py file to the basic syntax section

---
+++ 01.BASIC SYNTAX/06. strings.py
@@ -0,0 +1,14 @@
+part1 = "Stay hungry."
+#part1 is a string variable
+# "Stay hungry." is a string literal that contains the text to be printed
+# part1 is a variable that stores the string "Stay hungry."
+################################################################################################
+part2 = "Stay foolish."
+# part2 is a string variable
+# "Stay foolish ." is a string literal that contains the text to be printed
+# part2 is a variable that stores the string "Stay foolish."
+################################################################################################
+
+quote = part1 +" " + part2
+
+print(quote)
