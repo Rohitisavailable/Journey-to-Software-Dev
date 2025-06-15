245f6fb2e8f4380e24baff86295aa0429ec4e00d
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-15 13:15:25 +0530

    addded new string example to strings.py

--- 01.BASIC SYNTAX/06. strings.py
+++ 01.BASIC SYNTAX/06. strings.py
@@ -11,4 +11,25 @@ part2 = "Stay foolish."
 
 quote = part1 +" " + part2
 
+#print(quote)
+# The above line concatenates part1 and part2 with a space in between
+################################################################################################
+quote = f"{part1} {part2}"
+# f-string is a way to format strings in Python
+# It allows you to embed expressions inside string literals, using curly braces {}
+# # The f-string is prefixed with the letter 'f'
+# This allows you to include variables and expressions directly in the string'
 print(quote)
+################################################################################################
+
+name = "Steve Jobs"
+# name is a string variable that stores the name "Steve Jobs"
+
+age = 56
+# age is an integer variable that stores the age 56
+
+work = "Apple Inc."
+# work is a string variable that stores the name of the company "Apple Inc."
+message = f"Your Name is {name} and your age is {age} and you work at {work}."
+# message is a string variable that stores a formatted message
+print(message)
