8afd9659865ea64a5f2b5154b48a2d044bfe1d98
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-16 17:54:48 +0530

    Added a new file `boolean.py` Added basic boolean operations in Python

---
+++ 01.BASIC SYNTAX/07. boolean.py
@@ -0,0 +1,34 @@
+is_empty = True 
+#is_empty is a boolean variable that indicates whether a container is empty or not.
+# It is set to True, meaning the container is currently empty.
+#boolean variables can be used to represent two states: True or False.
+# For example, it can be used to check if a list, string, or other data structure has no elements.
+#Boolean variables should be used using capital letters (True or False) in Python.
+# This is a common practice in programming to use boolean variables for conditions and checks.
+
+#value of True or False is determined by the context in which it is used.
+#value of True is 1
+
+is_hidden = False
+#is_hidden is a boolean variable that indicates whether something is hidden or not.
+# It is set to False, meaning the item is currently visible or not hidden.
+#boolean variables can be used to represent two states: True or False.
+# For example, it can be used to check if a user interface element is visible or not.
+
+#value of False is 0
+
+print(42 * is_hidden)
+
+print(42 * False)
+
+#in order to print the value of 0 we can use either False or is_hidden.
+#here, we are multiplying 42 by is_hidden which is False (0).
+# The result will be 0 because any number multiplied by 0 is 0.
+
+print(50 * is_empty)
+
+print(50 * True)
+#in order to print the value of 1 we can use either True or is_empty.
+#here, we are multiplying 50 by is_empty which is True (1).
+# The result will be 50 because any number multiplied by 1 is the number itself.
+
