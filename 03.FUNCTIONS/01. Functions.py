172d470e9b86234a423dbee564d1241c70c709c8
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-18 21:17:08 +0530

    added new function to calculate the area of a rectangle

---
+++ 03.FUNCTIONS/01. Functions.py
@@ -0,0 +1,10 @@
+#funcions are reusable pieces of code that can be called with different arguments to perform a specific task.
+
+print("Rectangle Area Calculator")
+
+length = 5
+width = 4 
+
+area = length * width
+
+print(f"The area of the rectangle with lenght {length} and width {width} is {area}.")
