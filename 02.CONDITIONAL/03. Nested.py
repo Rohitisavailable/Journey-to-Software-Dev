f2315420b20741c06b72c857ff08d3824c089d1e
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-18 14:41:01 +0530

    added new Lines

---
+++ 02.CONDITIONAL/03. Nested.py
@@ -0,0 +1,16 @@
+#if condition:
+    # code to run when condition is True
+    # if other_condition:
+    # code to run when other_condition is True
+    #elif:
+        #code 
+    #else:
+        #code
+        
+        
+        
+#caution: avoid deeply nested code 
+#don't go beyond 2-3 levels of nesting otherwise it becomes hard to read and maintain
+
+
+
