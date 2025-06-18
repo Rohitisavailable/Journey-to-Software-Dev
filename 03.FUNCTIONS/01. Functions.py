d764236e4ed590e6bde394f3e09cd5ad1d1caa2e
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-18 21:19:01 +0530

    added documentation for functions

--- 03.FUNCTIONS/01. Functions.py
+++ 03.FUNCTIONS/01. Functions.py
@@ -3,8 +3,15 @@
 print("Rectangle Area Calculator")
 
 length = 5
+#length is a variable that stores the length of the rectangle
 width = 4 
+#width is a variable that stores the width of the rectangle
 
 area = length * width
+#area is a variable that stores the area of the rectangle calculated by multiplying length and width
+# * is the multiplication operator in Python
+# The area of a rectangle is calculated as length multiplied by width
 
 print(f"The area of the rectangle with lenght {length} and width {width} is {area}.")
+# f-string is used to format the string with the values of length, width, and area
+#{} is used to insert the value of a variable into a string
