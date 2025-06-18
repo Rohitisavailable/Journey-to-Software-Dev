9b6c7340cf3f8f2b6ebbeb85ed918f7235b01146
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-18 21:32:30 +0530

    added  summary for functions

--- 03.FUNCTIONS/01. Functions.py
+++ 03.FUNCTIONS/01. Functions.py
@@ -15,3 +15,23 @@ area = length * width
 print(f"The area of the rectangle with lenght {length} and width {width} is {area}.")
 # f-string is used to format the string with the values of length, width, and area
 #{} is used to insert the value of a variable into a string
+################################################################################################################
+#def is a keyword used to define a function in Python
+#def function_name(parameters1, parameters2, .... parameters_n):
+#function body 
+  #return value
+#put space after def and specify the function name 
+#funtion name should be lowercase 
+#use underscores to separate words 
+#name should be descriptive 
+#start function name with a verb like :
+# 1. authenticate,
+# 2. calculate,
+# 3. get,
+# 4. set,
+# 5. update,
+# 6. delete,
+# 7. create,
+# 8. find,
+# 9. print,
+# 10. show
