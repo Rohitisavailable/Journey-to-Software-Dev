57c17c86f2c57a9fdcafe9da29709a76c967bfb3
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-21 15:42:27 +0530

    .

--- 03.FUNCTIONS/02. Area calculator.py
+++ 03.FUNCTIONS/02. Area calculator.py
@@ -21,10 +21,27 @@ Select a shape
 """)
 
 selection = input("""\t'S' - Square
-                  \t'R - Rectangle)
-                  \t'C' - Circle
-                  """)
+\t'R - Rectangle
+\t'C' - Circle
+""")
 
 area = 0
 
-if selection ==  
+if selection == 'S':
+    side = input("Enter the side:")
+    area = calculate_square_area(float(side))
+    #float() converts the input string to a float for calculation
+
+elif selection == 'R':
+    length = input("Enter the length:")
+    width = input("Enter the width:")
+    area = calculate_rectangle_area(float(length), float(width))
+    #float() is used to convert the input strings to floats for calculation
+    
+elif selection == 'C':
+    radius = input("Enter the radius:")
+    area = calculate_circle_area(float(radius))
+else:
+    print("Invalid choose S,R,C")
+
+print(f"The area is {area}")
