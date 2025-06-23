88ca5ed6f38dcc48ba28c3e70c272883f589b186
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-23 17:47:04 +0530

    .

---
+++ 04.LOOPS/02. While.py
@@ -0,0 +1,12 @@
+#while expression:
+    #Code block that runs if expression is true
+#else:
+    # If expression is flase,
+    # run this codeand terminate loop
+    
+# while loop test a condition expression and runs the body of the loop while the condition remains true.
+    
+
+# Example of a while loop
+# while login_not_successful:
+    #ask for username and password
