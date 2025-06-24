1b33504336230779ec44c3653f77a8af8e1f68cf
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-24 21:49:37 +0530

    .

--- 04.LOOPS/03. Infinte.py
+++ 04.LOOPS/03. Infinte.py
@@ -12,7 +12,11 @@ passw = input("Enter your password:")
 
 attempt = 1
 max_attempt = 5
+is_authenticated = False 
+
 while login(user, passw) == False:
+    attempt += 1
+    if attempt > max_attempt: break
     print("Login failed, check again")
     # If the login is unsuccessful, ask for username and password again
     
