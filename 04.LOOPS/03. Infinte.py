040de6cd23ad999283c838882e337c1f98447bb6
Rohit Mahajan <tfrohit02@gmail.com>
2025-06-25 21:00:19 +0530

    .

--- 04.LOOPS/03. Infinte.py
+++ 04.LOOPS/03. Infinte.py
@@ -16,7 +16,12 @@ is_authenticated = False
 
 while login(user, passw) == False:
     attempt += 1
+    # += is a shorthand for attempt = attempt + 1
+    #that means we are incrementing the attempt by 1
+    #incrementing means increasing the value of a variable by a certain amount
     if attempt > max_attempt: break
+    #if attempt is greater than max_attempt, we break the loop
+    #break is a keyword that is used to exit a loop 
     #break terminates the loop
     print("Login failed, check again")
     
