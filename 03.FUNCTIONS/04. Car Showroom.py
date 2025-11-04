94884ae33c41e562bda45d770b39b20c9cf16f87
Rohit Mahajan <tfrohit02@gmail.com>
2025-11-04 11:10:17 +0530

    added a car name function and improved formatting in car showroom script

--- 03.FUNCTIONS/04. Car Showroom.py
+++ 03.FUNCTIONS/04. Car Showroom.py
@@ -14,19 +14,20 @@ print("""
 Select a Car
 --------------
 """)
-
+#\t is used for tab space
 selection = input("""
 ---------------------------------------
-\t'1' - BMW
+\t'1' - BMW 
 \t'2' - Ferrari
 \t'3' - Lamborgini
 ---------------------------------------
 """)
 
 def select_color(selection):
+    #unkown is used as a default value to avoid errors
     color = "Unknown"
     if selection == '1':
-        color = input("Enter the color for your BMW:")
+        color = input("Enter the color for your BMW:") #input function to take user input
     elif selection == '2':
         color = input("Enter the color for your Ferrari:")
     elif selection == '3':
@@ -35,7 +36,7 @@ def select_color(selection):
         print("Invalid choice. Please select 1, 2, or 3.")
     return color
 
-def get_car_name(tag):
+def get_car_name(tag): #def is used to define a function, get_car_name is the function name, tag is the parameter
     car = "Unknwn"
     if tag == '1':
         car = "BMW"
@@ -46,3 +47,4 @@ def get_car_name(tag):
     return car
 
 print(f"Your {get_car_name(selection)} with color {select_color(selection)} will be delivered in 7-10 days.")
+ #f-string is used to format the string with variables,get_car_name(selection) calls the function to get the car name based on user selection
