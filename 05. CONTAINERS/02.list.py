0b752439252d954277d3652b2173c1b4b6c0677e
Rohit Mahajan <tfrohit02@gmail.com>
2025-11-25 00:57:55 +0530

    klk

--- 05. CONTAINERS/02.list.py
+++ 05. CONTAINERS/02.list.py
@@ -3,7 +3,7 @@ primes = list()
 # list() is a built-in function that creates a list
 print(primes)
 
-primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+primes = []
 #primes is now a list of the first ten prime numbers
 #[2, 3, 5, 7, 11, 13, 17, 19, 23, 29] is a list literal that creates a list with the specified values
 
@@ -11,6 +11,10 @@ primes.append(2)
 primes.append(3)
 primes.append(5)
 primes.append(7)
+primes.append(11)
+primes.append(13)
+primes.append(17)
+primes.append(19)
 #append() is a list method that adds an element to the end of the list
 #Here we are adding the first four prime numbers to the list
 
