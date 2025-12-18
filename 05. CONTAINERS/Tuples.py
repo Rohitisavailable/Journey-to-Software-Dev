7eb0f59929a9c1aacdb769abbec7ea69d1cc326e
Rohit Mahajan <tfrohit02@gmail.com>
2025-12-18 13:33:27 +0530

    new tuples file added

---
+++ 05. CONTAINERS/Tuples.py
@@ -0,0 +1,12 @@
+chars = ("a", "b", "c")
+
+even_nums = (2, 4, 6, 8)
+
+
+voids = ()
+#voids is an empty tuple
+truths = (True,)
+#truths is a single-element tuple containing the boolean value True
+
+truths.append(False)
+#this will raise an AttributeError because tuples do not have an append() method
