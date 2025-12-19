505fea56897135efb00ed12ba01abfe6f148856d
Rohit Mahajan <tfrohit02@gmail.com>
2025-12-19 12:28:47 +0530

    new Dictionaries file

---
+++ 05. CONTAINERS/05.Dictionaries.py
@@ -0,0 +1,15 @@
+ssn_name_pairs = dict()
+#dict() creates an empty dictionary
+#dictionaries store data in key-value pairs
+#ssn_name_pairs is the name of the dictionary
+#here ssn_name_pairs will store social security numbers as keys and names as values
+
+ssn_name_pairs["123-45-6789"] = "Alice Smith"
+#this adds a key-value pair to the dictionary
+
+ssn_name_pairs["987-65-4321"] = "Bob Johnson"
+#adding another key-value pair
+
+ssn_name_pairs["555-55-5555"] = "Charlie Brown"
+#adding a third key-value pair
+
