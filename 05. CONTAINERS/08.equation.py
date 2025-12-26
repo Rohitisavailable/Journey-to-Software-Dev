ef75393478ab25ed3b9a89f9af2cee6adff3bec5
Rohit Mahajan <tfrohit02@gmail.com>
2025-12-26 21:17:01 +0530

    .

--- 05. CONTAINERS/08.equation.py
+++ 05. CONTAINERS/08.equation.py
@@ -1,11 +1,18 @@
 def solve_eqn(a: float, b : float, c:float) -> float:
+#def funtion_name(parm1: type, parm2: type, param3: type) -> return_type:
     return(float(c) - float(b)) / float(a)
+    #return to c - b divided by a
+    #x = (c - b) / a
 
 print("ax + b = c linear eqn")
+#we used ax for coefficient of x
+#example: 2x + 3 = 7
+#x = (c - b) / a
 
 a = input("Enter a: ")
 b = input("Enter b: ")
 c = input("Enter c: ")
 
 x = solve_eqn(a, b, c)
+#x is assigned the return value of solve_eqn function
 print(f"x is {x}")
