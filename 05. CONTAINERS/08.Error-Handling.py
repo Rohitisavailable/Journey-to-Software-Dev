46eff5ca34a301a492c8ccbc9f558975a4efa791
Rohit Mahajan <tfrohit02@gmail.com>
2025-12-26 21:34:37 +0530

    .

--- 05. CONTAINERS/08.Error-Handling.py
+++ 05. CONTAINERS/08.Error-Handling.py
@@ -1,9 +1,11 @@
 def solve_eqn(a: float, b : float, c:float) -> float:
+    try:
 #def funtion_name(parm1: type, parm2: type, param3: type) -> return_type:
-    return(float(c) - float(b)) / float(a)
+        return(float(c) - float(b)) / float(a)
     #return to c - b divided by a
     #x = (c - b) / a
-
+    except ZeroDivisionError:
+        print("error a cant be zero enter valid")
 print("ax + b = c linear eqn")
 #we used ax for coefficient of x
 #example: 2x + 3 = 7
@@ -16,3 +18,9 @@ c = input("Enter c: ")
 x = solve_eqn(a, b, c)
 #x is assigned the return value of solve_eqn function
 print(f"x is {x}")
+
+
+
+#summary:
+#error handling is a way to manage and respond to errors that may occur during the execution of a program.
+#there are different types of errors like syntax errors, runtime errors, logical errors etc.
