6f3786ef77af528bad0408be33b0ab0057af0312
Rohit Mahajan <tfrohit02@gmail.com>
2026-01-03 17:09:30 +0530

    .

--- 05. CONTAINERS/08.Error-Handling.py
+++ 05. CONTAINERS/08.Error-Handling.py
@@ -14,22 +14,36 @@ def solve_eqn(a: float, b : float, c:float) -> float:
         print("error 'a' cant be zero enter valid")
     except ValueError:
         print("error enter numeric value")
+
+
+    finally:
+       
+        #finally block will always execute regardless of whether an exception occurred or not
+       #it will execute after try and except blocks
+        print("Leaving solve_eqn function")
 print("ax + b = c linear eqn")
+
 
 #we used ax for coefficient of x
 #example: 2x + 3 = 7
 #x = (c - b) / a
 
 a = input("Enter a: ")
+if float(a) == 0:
+    raise ValueError("a cant be zero")
+
 b = input("Enter b: ")
 c = input("Enter c: ")
 
 try:
     x = solve_eqn(a, b, c)
 #x is assigned the return value of solve_eqn function
-print(f"x is {x}")
 
-
+except Exception:
+    #except block to catch any other exceptions that may occur
+    print("Something bad happened")
+else:
+    print(f"X is {x} ")
 
 #summary:
 #error handling is a way to manage and respond to errors that may occur during the execution of a program.
