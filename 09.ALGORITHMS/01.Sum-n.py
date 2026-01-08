4eaca98da9f625a1fd9ddc6d659ce62ea8b87e2c
Rohit Mahajan <tfrohit02@gmail.com>
2026-01-08 12:23:53 +0530

    .

---
+++ 09.ALGORITHMS/01.Sum-n.py
@@ -0,0 +1,20 @@
+import time
+
+
+
+
+def sum(n: int) -> int:
+    result = 0
+    for i in range(n + 1):
+        result += i
+    return result
+
+ranges = (10, 1000, 10000, 100000)
+
+
+for n in ranges:
+    start = time.process_time_ns()
+    sum(n)
+    end = time.process_time_ns()
+    exec_time = end - start
+    print(f"sum ({n}) execution time: {exec_time}ns")
