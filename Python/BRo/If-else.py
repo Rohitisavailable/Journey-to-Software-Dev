debefc48d1a624738850986f6e82a3e00bd370c9
Rohit Mahajan <tfrohit02@gmail.com>
2026-01-10 19:39:14 +0530

    .

---
+++ Python/BRo/If-else.py
@@ -0,0 +1,18 @@
+is_student = False
+for_sale = True
+is_online = True
+
+if is_student:
+    print("You can Enter")
+else:
+    print("You cannot Enter")
+
+if for_sale:
+    print("That item is for Sale")
+else:
+    print("You can Buy That")
+
+if is_online:
+    print("You are Online")
+else:
+    print("You are Offline")
---
+++ Typecasting.py
@@ -0,0 +1,29 @@
+#Typecasting = The process of converting a variable from one data type to another
+
+
+name = "Dawg"
+age = 25
+gpa = 8.4
+is_student = True
+
+
+print(f"the name is a {type(name)}")
+print(f"the age is a {type(age)}")
+print(f"the gpa is a {type(gpa)}")
+print(f"the is_student is a {type(is_student)}")
+#Converting
+
+gpa = int(gpa)
+
+print(gpa)
+#it converts 8.4 into a whole i.e 8
+
+age2 = float(age)
+print(age2)
+#it converted age i.e 25 and added it a decimal making it 25.0
+
+age1 = str(age)
+
+print(age1)
+#it will convert it into string not in integer
+print(type(age1))
