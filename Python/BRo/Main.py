bad77044f448be868cc293b84f44e4c4218d1724
Rohit Mahajan <tfrohit02@gmail.com>
2026-01-10 19:07:27 +0530

    .

---
+++ Python/BRo/Main.py
@@ -0,0 +1,35 @@
+# Variable = A conatainer for a value( string, integer, float, boolean)
+
+#Strings
+
+first_name = "Rob"
+food = "pizza"
+email = "Rob67@gmail.com"
+
+print(f"my name is {first_name}")
+print(f"i like {food}")
+print(f"Pls send mail at :{email}")
+
+#Integer
+
+age = 25
+quantity = 2
+num_of_friends = 15
+print(f"{first_name} is {age} years old")
+print(f"{first_name} bought {quantity} dominos {food} using {email} mail")
+print(f"you have {num_of_friends} friends saved in you Phine book")
+
+#Float
+
+price = 10.99
+gpa = 9.2
+
+print(f"Your {food} costs total {price}")
+print(f"{first_name} scored {gpa} in their University")
+
+
+#Boolean
+#True or False
+is_student = True
+
+print(f"{first_name} are you a Student: {is_student} ")
