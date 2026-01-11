353b51dd43aeae4da28e632ccab053ec90cf9032
Rohit Mahajan <tfrohit02@gmail.com>
2026-01-11 17:25:30 +0530

    .

---
+++ Python/BRo/Excercise/area.py
@@ -0,0 +1,9 @@
+import math
+
+radius = float(input("Enter the Radius of the Circle: "))
+
+diameter = radius * 2
+
+area = math.pi * diameter
+
+print(f"The area of the circle is: {round(area, 2)} and the radius is: {radius} and the Diameter is: {diameter}")
---
+++ Python/BRo/Excercise/circumference.py
@@ -0,0 +1,6 @@
+import  math
+radius = float(input("Enter the radius of Circle: "))
+
+circumference = 2 * math.pi * radius
+
+print(f"The circumference of the {radius} cm Circle is: {round(circumference, 2)} cm")
