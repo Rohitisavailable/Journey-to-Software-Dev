7fd0b3057754cd7c19cbfcb5ac16c882eee7bdb7
Rohit Mahajan <tfrohit02@gmail.com>
2026-01-11 15:26:28 +0530

    .

---
+++ Python/BRo/operators.py
@@ -0,0 +1,35 @@
+#ADDITION OPERATORS
+
+friends = 15
+
+friends = friends + 1
+friends += 1
+print(friends)
+
+
+#SUBSTRACTION OPERATORS
+friends = friends - 1
+friends -= 1
+
+print(friends)
+
+#MULTIPLICATION OPERATORS
+
+friends = friends * 3
+friends *= 3
+print(friends)
+
+#DIVISION OPERATORS
+
+friends = friends / 2
+friends /= 1
+print(friends)
+
+#EXPOTENT OPERATORS
+#" ** represents TO the power"
+friends = friends ** 2
+friends **= 2
+
+print(friends)
+
+#
