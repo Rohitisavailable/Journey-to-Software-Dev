bb3ba69127f2d4fb708e28eb23611ad17829ac93
Rohit Mahajan <tfrohit02@gmail.com>
2026-01-13 16:50:45 +0530

    .

---
+++ Python/BRo/if.py
@@ -0,0 +1,9 @@
+# if = Do some code only if some condition is true, else do nothing
+
+age = int(input("Enter Your Age: "))
+
+if age == 18:
+    print(f"Your age is 18")
+
+else:
+    print("Your age is not 18")
