f78762935cd3ad6afdbaa27811ce3ae9623530b5
Rohit Mahajan <tfrohit02@gmail.com>
2026-01-15 19:03:34 +0530

    .

--- Python/BRo/logic.py
+++ Python/BRo/logic.py
@@ -3,7 +3,9 @@
 # and = both must be true
 # not = inverts the condition
 
-temp = 45
+
+# OR OPERATOR 
+temp = 25
 
 is_raining = False
 #            ▼ or operator
@@ -12,3 +14,28 @@ if temp > 35 or temp < 0 or is_raining:
 
 else:
     print("not cancel")
+
+# AND OPERATOR
+
+temperature = -10
+
+is_snowing = True
+
+if temperature < 0 and is_snowing:
+    print("It is Winter")
+
+else:
+    print("it is Summer")
+
+
+#    NOT OPERATOR
+
+temp1 = 156
+
+is_burining = True
+
+if temp1 <= 15 and not is_burining:
+    print("it is cold")
+
+else:
+    print("it is hot")
